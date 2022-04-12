657829165689083103651e98a45c79cecc20fbcc
zübeyir <zeynelzatari@gmail.com>
2022-04-12 12:46:48 +0200

    day38 Inheritance continue - Method Overriding

---
+++ src/day38_Inheritance/carTask/Parking.java
@@ -0,0 +1,17 @@
+package day38_Inheritance.carTask;
+
+public class Parking {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Corolla","Black",2018,20000,20000);
+
+        Tesla tesla = new Tesla("Model S","Red",2022,95500,0);
+
+        BMW bmw = new BMW("X5","black",2022,45000,10000);
+
+
+
+
+    }
+}

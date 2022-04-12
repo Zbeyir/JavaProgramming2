72c6e1a9bb1f258749a6eac94df16ca2ecfc0cee
zübeyir <zeynelzatari@gmail.com>
2022-04-12 05:54:03 +0200

    day38 Inheritance continue - Method Overriding

---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,16 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends Car {
+
+
+    public Toyota(String brand, String model, String color, int year, double price, int miles) {
+        super(brand, model, color, year, price, miles);
+    }
+}
+
+/*
+Create the following sub classes of Car:
+			1. Toyota:
+					extra methods:
+							reliabile()
+ */

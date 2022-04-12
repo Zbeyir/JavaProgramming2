523e1547abc445ae17e259aae729e3879cd693a3
zübeyir <zeynelzatari@gmail.com>
2022-04-12 19:50:25 +0200

    day39_Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -1,8 +1,48 @@
 package day39_Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape {
 
     private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if (side <= 0){
+            System.err.println("Invalid side: " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+
+    // constructor u burada kendimiz secerek yaptik yukarida ki compiler err gidermek icin
+    public Square(double side) {
+        super("Square");  // diger yerlerde yaptigimz gibi yukaridan parentez icinden String i sildik buraya direk adini yazdik
+       setSide(side);  // buraya da setSide kendimiz yazdik (sistem kendisi otomatik 'this.side = side' ekliyor)
+    }
+
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+
 
 }
 

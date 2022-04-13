3a9ff06253f148e15b8f7014ffd5aa7f91dcf6db
zübeyir <zeynelzatari@gmail.com>
2022-04-13 18:09:23 +0200

    day40_FinalKeyword

---
+++ src/day40_FinalKeyword/EncapsulationReview.java
@@ -0,0 +1,38 @@
+package day40_FinalKeyword;
+
+import day39_Recap.shapeTask.Circle;
+import day39_Recap.shapeTask.Square;
+
+public class EncapsulationReview {
+
+    private Circle circle;
+
+    private Square square;
+
+    public Circle getCircle() {
+        return circle;
+    }
+
+    public void setCircle(Circle circle) {
+        if (circle.getRadius() < 5 ){
+            return;
+        }
+        this.circle = circle;
+    }
+
+
+    public Square getSquare() {
+        return square;
+    }
+
+    public void setSquare(Square square) {
+        this.square = square;
+    }
+
+
+
+}
+/*
+mesela getter ve setter larin basina (access modifier)
+ private yazsak baska yerde kullanamyiz sadece ayni sinif ta kullanbilirz
+ */

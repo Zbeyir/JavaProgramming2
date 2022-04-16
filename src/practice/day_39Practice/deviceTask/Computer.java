f2909adaf60a5c798efa3c2d5a84853384b0ec85
zübeyir <zeynelzatari@gmail.com>
2022-04-16 21:43:38 +0200

    Day 39 practice Device Task

---
+++ src/practice/day_39Practice/deviceTask/Computer.java
@@ -0,0 +1,18 @@
+package practice.day_39Practice.deviceTask;
+
+public class Computer extends Device {
+
+    public Computer(String brand, String model, double price, boolean hasBattery, boolean hasPowerButton) {
+        super(brand, model, price, hasBattery, hasPowerButton);
+    }
+
+
+
+}
+/*
+3. Create a subclass of Device named Computer:
+
+				Add the extra methods if needed
+
+
+ */

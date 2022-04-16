9757a5493f075c04e2eae6c0c4d477a4304fa7dd
zübeyir <zeynelzatari@gmail.com>
2022-04-16 21:41:13 +0200

    Day 39 practice Device Task

---
+++ src/practice/day_39Practice/deviceTask/Phone.java
@@ -0,0 +1,24 @@
+package practice.day_39Practice.deviceTask;
+
+public class Phone extends Device {
+
+    public Phone(String brand, String model, double price, boolean hasBattery, boolean hasPowerButton) {
+        super(brand, model, price, hasBattery, hasPowerButton);
+    }
+
+    public void call(long phoneNum){
+        System.out.println(getBrand() + " " + getModel() + " is calling " + phoneNum);
+    }
+
+    public void text(long phoneNum){
+        System.out.println(getBrand() + " " + getModel() + " is texting " + phoneNum);
+    }
+
+}
+/*
+2. Create a subclass of Device named Phone:
+
+				Extra Methods:
+						call(long phoneNum), text(long phoneNum)
+
+ */

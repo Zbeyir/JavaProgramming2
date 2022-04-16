8f552e5820293ffcfc81bcb652b295cf58f45dfe
zübeyir <zeynelzatari@gmail.com>
2022-04-16 19:40:04 +0200

    Day 39 practice animal task

---
+++ src/practice/day_39Practice/animalTask/Crocodile.java
@@ -0,0 +1,25 @@
+package practice.day_39Practice.animalTask;
+
+public class Crocodile extends WildAnimal{
+
+    public Crocodile(String name, String breed, String size, String color, int age, char gender, boolean isWild, boolean isFriendly, boolean isPlayable) {
+        super(name, breed, size, color, age, gender, isWild, isFriendly, isPlayable);
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("Crocodile " + getBreed() + " is hunts buffalo");
+    }
+
+
+}
+
+/*
+5. Create the following sub classes of WildAnimal and Override the hunt method, and add any extra methods that are needed:
+				1. Lion
+				2. Tiger
+				3. Eagle
+				4. Bear
+				5. Python
+				6. Crocodile
+ */

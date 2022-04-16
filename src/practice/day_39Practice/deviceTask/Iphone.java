e7ad08ca01ae0565ad4fc048368aa770abedfec9
zübeyir <zeynelzatari@gmail.com>
2022-04-16 21:51:46 +0200

    Day 39 practice Device Task

---
+++ src/practice/day_39Practice/deviceTask/Iphone.java
@@ -0,0 +1,21 @@
+package practice.day_39Practice.deviceTask;
+
+public class Iphone extends Device{
+
+    public Iphone(String brand, String model, double price, boolean hasBattery, boolean hasPowerButton) {
+        super(brand, model, price, hasBattery, hasPowerButton);
+    }
+
+}
+
+/*
+5. Create the following sub classes of Phone and add any extra methods that are needed:
+
+				1. Iphone
+				2. Samsung
+				3. BlackBerry
+				4. Google
+				5. Nokia
+
+
+ */

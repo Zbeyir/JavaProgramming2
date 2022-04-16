f8bc98c3a630c013edaf135622806af8d3d00369
zübeyir <zeynelzatari@gmail.com>
2022-04-16 22:35:07 +0200

    Day 39 practice states Task

---
+++ src/practice/day_39Practice/statesTask/California.java
@@ -0,0 +1,18 @@
+package practice.day_39Practice.statesTask;
+
+public class California extends States{
+
+    public California(String name, String abbreviation, String politicalParty, String governor, String senator, int population, double stateTax) {
+        super(name, abbreviation, politicalParty, governor, senator, population, stateTax);
+    }
+
+}
+/*
+2. Create the following sub classes of States and add any additional fields and methods if necessary:
+				1. Virginia
+				2. California
+				3. Texas
+				4. Florida
+
+
+ */

2f50ba2e7ed476f2741e348783ed3e45aa790253
zübeyir <zeynelzatari@gmail.com>
2022-04-16 19:21:03 +0200

    Day 39 practice animal task

---
+++ src/practice/day_39Practice/animalTask/Parrot.java
@@ -0,0 +1,26 @@
+package practice.day_39Practice.animalTask;
+
+public class Parrot extends FriendlyAnimal {
+
+    public Parrot(String name, String breed, String size, String color, int age, char gender, boolean isWild, boolean isFriendly, boolean isPlayable) {
+        super(name, breed, size, color, age, gender, isWild, isFriendly, isPlayable);
+    }
+
+    public void fly(){
+        System.out.println(getName() + " " + getBreed() + " is flying");
+    }
+
+    public void sing(){
+        System.out.println(getName() + " " + getBreed() + " is singing");
+    }
+
+}
+/*
+4. Create the following sub classes of FriendlyAnimal and Override the eat method
+
+
+			4. Parrot:
+					Extra methods:
+						fly()
+						sing()
+ */

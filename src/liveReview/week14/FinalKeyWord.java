6ae9c5c0df7ce9f92fec526d8a81e4e82b451b73
zübeyir <zeynelzatari@gmail.com>
2022-04-17 16:58:09 +0200

    week 12

--- src/liveReview/week14/FinalKeyWord.java
+++ src/liveReview/week14/FinalKeyWord.java
@@ -1,5 +1,7 @@
 package liveReview.week14;
 
+import java.util.ArrayList;
+
 public class FinalKeyWord {
 
     final int b; // initialization is  mandatory
@@ -20,6 +22,29 @@ public class FinalKeyWord {
         // local variables: if we don't use it, initialization is not mandatory
         final int a;
         a = 100;
+
+        // Final Arrays
+
+        final int[] nums = {1,2,3};
+        System.out.println(nums[0]);
+        nums[0] = 5;
+        System.out.println(nums[0]);
+
+         // nums = new int[] {5,6,7}; // we can not assign a new array object to our array reference variable
+
+        // Final Arraylist
+        final ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Orange");
+        colors.add("Red");
+
+        colors.remove(0);
+
+       // colors = new ArrayList<>(); // ==> Error
+
+        // What si becoming final :
+        // final key word is used for 'Variable'
+
 
 
     }

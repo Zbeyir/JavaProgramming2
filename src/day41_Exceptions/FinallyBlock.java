17135b2f3e45ebefa30857c8c2262b45b9c0e31a
zübeyir <zeynelzatari@gmail.com>
2022-04-18 19:58:03 +0200

    day41_Exceptions

---
+++ src/day41_Exceptions/FinallyBlock.java
@@ -0,0 +1,23 @@
+package day41_Exceptions;
+
+public class FinallyBlock {
+
+    public static void main(String[] args) {
+
+
+        int[] arr ={1,2,3};
+
+        try{
+            System.out.println(arr[10]);
+            System.out.println("Try Block"); // burasi ni cikti olarak alamiyoruz
+        } catch (RuntimeException e) {
+            System.out.println("Catch block"); // burasinai da yanlis yazarsak (parantez in cicni yani ) cikti olarak alamiyoz
+            e.printStackTrace();
+          //  System.exit(0); // this only time not executed (sadece bu durumda cikti olarak alamayiz)
+        }finally {
+            System.out.println("Finally Block"); // ama burasi her durum da yazdirilacaktir
+        }
+
+
+    }
+}

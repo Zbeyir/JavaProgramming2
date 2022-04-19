83bdf88887e64f043ca3d2eaa4398113c27c9fc9
zübeyir <zeynelzatari@gmail.com>
2022-04-19 21:22:48 +0200

    day42_Exceptions

---
+++ src/day42_Exceptions/CustomExceptions.java
@@ -0,0 +1,39 @@
+package day42_Exceptions;
+
+
+class FadyException extends RuntimeException{
+
+    public FadyException(String message) { // burada constructor yapmaz isek assagida sag tarafta aciklam alamayiz
+        super(message);                     // ve super key word kullanmaliyiz
+                                           //custom checked exception
+    }
+
+    public FadyException() {
+        super("Time for a short break");
+    }
+}
+
+class NoBreakException extends Exception{ //custom checked exception
+    // cheked exception oldugu icin bu method u cagirinca asssagida try catch yaptik
+
+
+}
+
+
+public class CustomExceptions {
+
+    public static void main(String[] args) {
+
+       // throw new FadyException();
+
+      //  throw new FadyException("It's time for lunch");
+
+        try {
+            throw new NoBreakException();
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}

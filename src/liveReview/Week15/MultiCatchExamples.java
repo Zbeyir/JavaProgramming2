98fee982f8174020be386cb70521e1fd1c3d1649
zübeyir <zeynelzatari@gmail.com>
2022-04-24 23:53:19 +0200

    Week 15

--- src/liveReview/Week15/MultiCatchExamples.java
+++ src/liveReview/Week15/MultiCatchExamples.java
@@ -9,12 +9,14 @@ public class MultiCatchExamples {
 
         try {
             System.out.println(word.substring(20)); // unchecked exception
-        } catch (StringIndexOutOfBoundsException e) {
+        } catch (StringIndexOutOfBoundsException e) {   //  1111
             System.out.println(e.getClass().getSimpleName());
             System.out.println("Reason = " + e.getMessage());
         }catch (Throwable e){   // hiyerarside ki en tepe
             System.out.println(e.getClass().getSimpleName());
             System.out.println("Reason = " + e.getMessage());
+       // } catch (StringIndexOutOfBoundsException e) {   // 11111---eger yukaridakine buray yazdugugumuzda
+            //hata vercek yukardan silip yazsan da birsey degismeyecek
 
         }
 

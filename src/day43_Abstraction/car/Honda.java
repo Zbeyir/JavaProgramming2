3749aa39d36982fe5bc26c59aafe459ebdc2cd91
zübeyir <zeynelzatari@gmail.com>
2022-04-26 15:09:00 +0200

    day43_Abstraction

--- src/day43_Abstraction/car/Honda.java
+++ src/day43_Abstraction/car/Honda.java
@@ -3,7 +3,7 @@ package day43_Abstraction.car;
 // burasi extends den sonra alti kirimizi oluyor hata verioyr
 // cünkü constructor ve abstract olan methodlar override yapilmali
 //parent class ta tanimlanan abstractlar sub class da bitrilmeli(tamamlanmali yani)
-public class Honda extends Car {
+public final class Honda extends Car {
 
 
     public Honda(String brand, String model, String color, int year, double price) {

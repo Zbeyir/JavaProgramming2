feeaa034d257b49a0565974adf3908c874276841
zübeyir <zeynelzatari@gmail.com>
2022-05-04 14:56:16 +0200

    day45_Abstraction

---
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -0,0 +1,47 @@
+package day45_Abstraction;
+
+public interface PropertiesOfInterface {
+
+    int a =100; // ama bunu int a; olarak tanimlamayiz ==> static & final by default
+
+    static int b = 200; // bunu da  static int b; olarak tanimlamayiz ==> final by default
+
+
+    /*
+    public PropertiesOfInterface(int a) {
+
+
+
+        this.a = a;
+    }
+    */
+
+   /*
+    static {
+        b=100;
+    }
+
+    */
+
+   /*
+    public void method1(){
+        System.out.println("Instance method");
+    }
+     *** no variable means no Instance method
+    */
+
+    public static void method2(){
+        System.out.println("static method");
+    }
+
+    public abstract void method3();
+
+
+    //default sildigimiz de hata verecek , ccünkü silince instance methoda dönecek
+    public default void method4(){
+        System.out.println("Default method");
+    }
+
+
+
+}

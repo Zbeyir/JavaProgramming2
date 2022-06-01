555731a1500a1d9b78e69a1044656f8d21a6516c
zübeyir <zeynelzatari@gmail.com>
2022-06-01 07:28:51 +0200

    day53_FunctionalInterface

--- src/day53_FunctionalInterface/BuildInFunctionalInterfaces1.java
+++ src/day53_FunctionalInterface/BuildInFunctionalInterfaces1.java
@@ -3,6 +3,8 @@ package day53_FunctionalInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class BuildInFunctionalInterfaces1 {
@@ -45,6 +47,106 @@ public class BuildInFunctionalInterfaces1 {
         System.out.println(names);
 
 
+        System.out.println("==========================================================");
+
+        Consumer<String> printEach = (s) -> {
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println(s.charAt(i));
+            }
+        };
+
+        printEach.accept("Java");
+
+
+        System.out.println("==========================================================");
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,34,5,6,7,8,9));
+
+        /*
+        for (Integer each : list2) {
+            if (each %2 != 0){
+                System.out.println(each);
+            }
+        }
+         */
+
+        System.out.println("each elements");
+        list2.forEach( p -> System.out.println(p) );
+
+        System.out.println("each odd numbers");
+        list2.forEach( p -> { if(p%2!=0) System.out.println(p); } );
+
+
+        System.out.println("==========================================================");
+
+        List<String> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList("Anel Adilbayeva","Aygun Abbasova Jarullah","Jilili Jiwuer","Ali Ismayilov"));
+
+        employees.forEach(s -> System.out.println(s.charAt(0) + "." + s.charAt(s.lastIndexOf(" ")+1)) );
+
+
+        System.out.println("==========================================================");
+
+
+        Function<int[], List<Integer>> convertToList = (a) -> {
+            List<Integer> result = new ArrayList<>();
+            for (int each : a) {
+                result.add(each);
+            }
+            return result;
+        };
+
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        List<Integer> l = convertToList.apply(arr);
+
+        System.out.println(l);
+
+         /*
+          cmd + sag tus ile klik methodlarin icine grip bakabiliyorsun
+          ve ne istemis veya neye döndürülüyor girip inceleyebilisin
+          mesela yukarida convertToList. yanina aplly yazdik bu apply Function FunctionalInterface den geliyor
+          gir bak icine görürüsün.
+    bir de bu Function FunctionalInterface de 2 tane deger girebiliyorsun yukarida ki gi
+
+        @FunctionalInterface
+        public interface Function<T, R> {
+            R apply(T t);
+                 */
+
+
+        System.out.println("==========================================================");
+
+        Function<List<Integer>, int[]> convertToArray = (a) -> {
+            int[] result = new int[a.size()];
+            for (int i = 0; i < a.size(); i++) {
+                result[i] = a.get(i);
+            }
+            return result;
+        };
+
+        List<Integer> nunbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+
+        int [] result2 = convertToArray.apply(nunbers);
+
+        System.out.println(Arrays.toString(result2));
+
+
+        // create a function that can return the maximum number from an int array
+
+        // create a function that can swap the first and last elements of an array
+
+        // Create a function that can reverse an array and returns it
+
+        // create a function that can reverse a List
+
+
+
+
 
     }
+
 }
+
+/*
+
+ */
